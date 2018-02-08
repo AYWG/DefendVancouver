44e80ad95ac9d705923b22a30db78bb411cd9784
Unknown <tanwin.nun@gmail.com>
2018-02-07 17:00:59 -0800

    Fixed stuff

---
+++ src/world.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Tanwin Nun on 2018-02-07.
+//
+
+#ifndef DEFENDVANCOUVER_WORLD_HPP
+#define DEFENDVANCOUVER_WORLD_HPP
+
+
+class world {
+
+};
+
+
+#endif //DEFENDVANCOUVER_WORLD_HPP
