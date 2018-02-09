7cda3c42893942c586daba9d1fd8725b2e6449b0
Unknown <tanwin.nun@gmail.com>
2018-02-08 16:18:49 -0800

    world.cpp made

--- src/world.cpp
+++ src/world.cpp
@@ -2,4 +2,59 @@
 // Created by Tanwin Nun on 2018-02-07.
 //
 
+// Header
 #include "world.hpp"
+
+// stlib
+#include <iostream>
+#include <string.h>
+#include <cassert>
+#include <sstream>
+#include <math.h>
+
+
+// Same as static in c, local to compilation unit
+namespace
+{
+
+}
+
+world::world() :
+{
+
+}
+
+world::~world()
+{
+
+}
+
+// World initialization
+bool world::init(vec2 screen)
+{
+
+}
+
+// Releases all the associated resources
+void world::destroy()
+{
+
+}
+
+// Update our game world
+bool world::update(float elapsed_ms)
+{
+
+}
+
+// Render our game world
+void world::draw()
+{
+
+}
+
+// Should the game be over ?
+bool world::is_over()const
+{
+
+}
