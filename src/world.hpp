c5b1b94e6bd14e34603ba68fd69867c1b2c71650
Unknown <tanwin.nun@gmail.com>
2018-02-08 16:12:40 -0800

    World & Common cpp & hpp made

--- src/world.hpp
+++ src/world.hpp
@@ -5,9 +5,32 @@
 #ifndef DEFENDVANCOUVER_WORLD_HPP
 #define DEFENDVANCOUVER_WORLD_HPP
 
+#pragma once
+
+// stlib
+#include <vector>
+#include <random>
+#include "common.hpp"
 
 class world {
+public:
+    world();
+    ~world();
 
+    // Creates a window, sets up events and begins the game
+    bool init(vec2 screen);
+
+    // Releases all associated resources
+    void destroy();
+
+    // Steps the game ahead by ms milliseconds
+    bool update(float ms);
+
+    // Renders our scene
+    void draw();
+
+    // Should the game be over ?
+    bool is_over()const;
 };
 
 
