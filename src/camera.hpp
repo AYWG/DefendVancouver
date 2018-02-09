f4db2f50ca9f2bfb554528bfc9aa0feb92734305
Andy Wong <andygbwong@gmail.com>
2018-02-08 18:06:20 -0800

    Added basic camera logic

--- src/camera.hpp
+++ src/camera.hpp
@@ -4,9 +4,29 @@
 
 #pragma once
 
+#include "common.hpp"
 
 class Camera {
+public:
+    /**
+     * Moves the camera to newPos
+     * @param newPos
+     */
+    void move(vec2 newPos);
 
+    /**
+     * Returns the position of the camera's origin relative to the world - this is the top left of the camera
+     * @return
+     */
+    vec2 getOrigin();
+
+
+private:
+    // The dimensions of the camera - should be the same as the window
+    vec2 m_size;
+
+    // The position of the camera in the world - this corresponds to a point in the center of the camera
+    vec2 m_pos;
 };
 
 
