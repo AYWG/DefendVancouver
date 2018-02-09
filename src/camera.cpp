0f6a35232360eaac487f10fad0c8849d0bb9c03c
Andy Wong <andygbwong@gmail.com>
2018-02-08 18:06:20 -0800

    Added basic camera logic

--- src/camera.cpp
+++ src/camera.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "camera.hpp"
+
+void Camera::move(vec2 newPos) {
+    m_pos = newPos;
+}
+
+vec2 Camera::getOrigin() {
+    return {m_pos.x - (m_size.x / 2.f), m_pos.y - (m_size.y / 2)};
+}
