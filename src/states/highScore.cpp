90d75afca5f788ba209488a2804a732befd1e2a2
Unknown <tanwin.nun@gmail.com>
2018-04-14 18:55:09 -0700

    State Machine implemented

---
+++ src/states/highScore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tanwin Nun on 2018-04-14.
+//
+
+#include "highScore.hpp"
---
+++ src/states/highScore.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Tanwin Nun on 2018-04-14.
+//
+
+#ifndef DEFENDVANCOUVER_HIGHSCORE_HPP
+#define DEFENDVANCOUVER_HIGHSCORE_HPP
+
+
+
+class highScore {
+
+};
+
+
+
+#endif //DEFENDVANCOUVER_HIGHSCORE_HPP
